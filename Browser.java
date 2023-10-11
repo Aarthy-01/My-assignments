9cfcb3f697ad0624a6bb326a880d4d36142bf61e
Aarthy-01 <147586630+Aarthy-01@users.noreply.github.com>
2023-10-11 12:28:22 +0530

    Add files via upload
    day1 assignments completed

---
+++ Browser.java
@@ -0,0 +1,10 @@
+package week1.day1.homework;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		
+System.out.println("THIS IS MY BROWSER");
+	}
+
+}
---
+++ Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1.homework;
+
+public class Chrome {
+	public void getName() {
+		
+
+	}
+	public void printName() {
+		System.out.println("This is google chrome");
+	}
+	public static void main(String[] args) {
+		Chrome chrome1=new Chrome();
+		 chrome1.getName();
+		 chrome1. printName();
+		 
+	}
+
+}
